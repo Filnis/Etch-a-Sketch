bfd0b4c94b08278e224defb64a6c410e7f5063c0
Filnis <filippo.nisoli@gmail.com>
2023-10-01 22:48:20 +0200

    Adds greyscale function

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-console.log("hello");
-
 let squareSize = document.getElementById("squareSize");
 let container = document.querySelector(".container");
+let label = document.getElementById("label1");
 
 // display the starting grid size value when loading the page
-document.getElementById("sliderValue").textContent = squareSize.value;
+label.textContent = `Grid size: ${squareSize.value}`; 
 generateGrid(squareSize.value);
 
 // display the new grid size value when moving the slider
 squareSize.addEventListener("change", function(){ 
-    document.getElementById("sliderValue").textContent = squareSize.value;
+    label.textContent = `Grid size: ${squareSize.value}`;
     generateGrid(squareSize.value);
 })
 
@@ -23,7 +22,7 @@ function generateGrid(size) {
         for(let j=0; j<size; j++){
             let square = document.createElement("div");
             square.className = "square";
-            square.style.flexBasis = Math.floor(100/size)-0.2 + "%";
+            square.style.flexBasis =  100/size + "%";
             container.appendChild(square);
         }
     }
@@ -33,6 +32,18 @@ function generateGrid(size) {
     squares.forEach(el => el.addEventListener("mouseover", changeColor));
 }
 
+// function changeColor(e) {
+//     let randomColor = Math.floor(Math.random()*16777215).toString(16);
+//     e.target.style.backgroundColor = "#" + randomColor;
+//     console.log(e.target.style.backgroundColor)
+// }
+
 function changeColor(e) {
-    e.target.style.backgroundColor = "red"
+    let currColor = window.getComputedStyle(e.target).backgroundColor;
+    let rgbValues = currColor.slice(4).slice(0, -1).split(", ");
+    let valueR = rgbValues[0] - 25.5;
+    let valueG = rgbValues[1] - 25.5;
+    let valueB = rgbValues[1] - 25.5;
+
+    e.target.style.backgroundColor =  `rgb(${valueR},${valueG},${valueB})`;
 }
